1067eb0e18ec595862971d0d829aa89ec18524b4
Pavel <p.filippov2.20@gmail.com>
2024-01-21 17:30:14 +0200

    feat:  06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,4 +1,3 @@
-// const { readFile } = require('fs');
 const fs = require('fs/promises');
 const path = require('path');
 
@@ -21,8 +20,8 @@ async function createDirectory() {
   // создаем 2 файла: index и styles
   try {
     await fs.writeFile(path.join(__dirname, 'project-dist/index.html'), 'index.html')
-    await fs.writeFile(path.join(__dirname, 'project-dist/styles.css'), 'styles.css')
-    console.log('file has been created!');
+    await fs.writeFile(path.join(__dirname, 'project-dist/style.css'), 'style.css')
+    console.log('files has been created!');
   } catch (error) {
     console.log(error)
   }
@@ -43,7 +42,7 @@ async function readAndReplaceTag() {
       .replace('{{articles}}', await fs.readFile(path.join(__dirname, 'components/articles.html'), 'utf-8'))
       .replace('{{footer}}', await fs.readFile(path.join(__dirname, 'components/footer.html'), 'utf-8'))
       .replace('{{header}}', await fs.readFile(path.join(__dirname, 'components/header.html'), 'utf-8'));
-    console.log(newContentTemplate)
+    // console.log(newContentTemplate)
     // переписываем index.html
     await fs.writeFile(path.join(__dirname, 'project-dist/index.html'), newContentTemplate)
 
@@ -52,3 +51,65 @@ async function readAndReplaceTag() {
   }
 }
 readAndReplaceTag()
+
+compilesStyles()
+async function compilesStyles() {
+  const dataArr = [];
+  const src = path.join(__dirname, 'styles');
+
+  try {
+    const files = await fs.readdir(src);
+    for (const file of files) {
+      if (file.endsWith('.css') && (await fs.stat(path.join(src, file))).isFile()) {
+        const filePath = path.join(src, file);
+        const fileContent = await fs.readFile(filePath, 'utf-8');
+        dataArr.push(fileContent);
+      }
+    }
+    dataArr.forEach((data) => {
+      // console.log(data);
+    });
+    return dataArr;
+  } catch (err) {
+    console.error('Error copying files:', err);
+  }
+}
+
+async function writeCssFile() {
+  const data = await compilesStyles();
+  const src = path.join(__dirname, 'project-dist/style.css');
+  try {
+    await fs.writeFile(src, data);
+  }
+  catch (err) {
+    console.error('Error copying file:', err);
+  }
+  console.log('All files are up-to-date!');
+}
+writeCssFile()
+
+
+// копируем директории, файлы и поддиректории с файлами
+async function copyDirectory(source, destination) {
+  try {
+    const entries = await fs.readdir(source, { withFileTypes: true });
+
+    for (const entry of entries) {
+      const sourcePath = path.join(source, entry.name);
+      const destPath = path.join(destination, entry.name);
+
+      if (entry.isDirectory()) {
+        await fs.mkdir(destPath, { recursive: true });
+        await copyDirectory(sourcePath, destPath);
+      } else {
+        await fs.copyFile(sourcePath, destPath);
+      }
+    }
+
+    console.log('Directory copied successfully!');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+copyDirectory(path.join(__dirname, 'assets'), path.join(__dirname, 'project-dist/assets'));
